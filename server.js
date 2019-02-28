57e85ffafb1f0825efde70a1f2f08e728806e6ff
Pawel Butra <pawel.butra@onet.pl>
2019-02-28 12:26:57 +0100

    Zadanie 3

--- server.js
+++ server.js
@@ -14,9 +14,21 @@ app.get('/userform', function (req, res) {
 	res.send(JSON.stringify(response));
 });
 
+app.get('/store', function (req, res) {
+	res.send("To jest sklep.");
+}, app.use( function(req, res, next) {
+	console.log("Jestem pośrednikiem przy żądaniu do /store!");
+	next();
+}));
+
+
 var server = app.listen(3000, 'localhost', function() {
     var host = server.address().address;
     var port = server.address().port;
 
     console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
 });
